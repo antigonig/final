febdbe8c50961dc2b081a4ba4d4203ac100ddf98
Antigoni <antigonisgarbis@gmail.com>
2023-08-14 09:22:09 -0500

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Antigoni Garbis on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
